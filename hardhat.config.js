7f6aa8bd2b1f0e970daf362108d41a0367da7ead
Daudit <dimo.dev@outlook.com>
2023-07-24 20:51:34 +0300

    feat(setup): Add multiple config files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,39 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("hardhat-deploy")
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
-  solidity: "0.8.19",
-};
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
+    },
+    defaultNetwork: "sepolia",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            saveDeployments: true,
+            chainId: 11155111,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+    },
+}
